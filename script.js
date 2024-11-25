3c5049c2c3e6b216fe09cb161b7b8aea964ab65e
Leo-Hamoudi <leohamoudi42@gmail.com>
2024-11-25 14:41:13 +0100

    essaie shifumi

---
+++ script.js
@@ -0,0 +1,31 @@
+// RAPPEL
+// Créer ma fonction
+// role : ce qu'elle doit faire : aditionner a et b
+// parametre ou arguments : ce dont elle a besoin pour travailler : a et b
+// retour : ce que la fonction va retourner, va nous donner : resultat de l'addition
+
+//Rôle: choix du joueur 
+//Paramètre: Pierre, Feuille, Ciseau
+
+function choixjoueur() {
+    return prompt("fait ton choix")
+}
+
+function parametreordi() {
+    return Math.ceil(Math.random() * 3)
+}
+
+function choixordi(nombre) {
+    let result;
+    if (nombre === 1) {
+        result = `papier`;
+    } else if (nombre === 2)}
+
+result = `feuille`; {
+            else if (nombre === 3)
+        result = `ciseaux`;
+}
+
+
+
+
